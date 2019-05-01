6dc65dc55c75ca0f518405e42939ae28ec8d6a51
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2019-04-30 23:12:48 -0700

    create post route register users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator/check');
 
 //@route GET api/users
 //@discription test route
 //@access public
-router.get('/', (req, res) => res.send('Users route'));
+router.post(
+  '/', 
+  [
+    check('name', 'name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'please include a valid email').isEmail(),
+    check('password', 'password must be 8 digits or more'
+    ).isLength({ min: 8 })
+  ], 
+  (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({ errors: errors.array() });
+  }
+  // console.log(req.body);
+    res.send('Users route')
+  }
+);
+   
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 //connect database
 connectDB();
 
+//init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res)=> res.send('API END HERE and working'));
 
 //define routes
