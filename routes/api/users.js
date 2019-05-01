603b3127d7b665bf7a4b42a796f5c0bb7c02c334
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2019-05-01 10:35:09 -0700

    create payload and jsonwebtoken sending to mongo

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const {check, validationResult} = require('express-validator/check');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../../models/User');
 
@@ -28,7 +30,7 @@ router.post(
       let user = await User.findOne({ email });
  //see if user exists
       if (user) {
-        res.status(400).json({ errors: [{ msg: 'user already exists'}] });
+        return res.status(400).json({ errors: [{ msg: 'user already exists'}] });
       }
   
   //get users avitar pictures NOT using yet
@@ -39,16 +41,29 @@ router.post(
     email,
     password
   });
-  //encrypt password
+
+//encrypt password
   const salt = await bcrypt.genSalt(10);
 
   user.password = await bcrypt.hash(password, salt);
   
   await user.save();
 
-  //return jsonwebtoken
+  const payload = {
+    user: {
+      id: user.id
+    }
+  };
 
-      res.send('Users route');
+  jwt.sign(
+    payload, 
+    config.get('jwtSecret'), 
+    { expiresIn: 36000 },
+    (err, token) => {
+      if (err) throw err;
+      res.json({token});
+  });
+
     } catch (err) {
        console.error(err.message);
        res.status(500).send('server error');
