8b2005e1f45c7ca91e538367f0dfa82c7714d2ac
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2019-05-01 21:14:53 -0700

    begin auth api and auth middleware instead of passport

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth')
 
 //@route GET api/users
 //@discription test route
-//@access public
-router.get('/', (req, res) => res.send('Auth route'));
+//@access private
+router.get('/', auth, (req, res) => res.send('Auth route'));
+
+
 
 module.exports = router;
