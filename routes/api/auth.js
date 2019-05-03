e4868bcd2cecd2c3519a843e9a709f89044dace9
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2019-05-02 20:40:09 -0700

    use token to get back all user data

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,10 +2,20 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth')
 
+const User = require('../../models/User');
+
 //@route GET api/users
 //@discription test route
-//@access private
-router.get('/', auth, (req, res) => res.send('Auth route'));
+//@access public
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch {
+      console.error(err.message);
+      res.status(500).send('server error');
+    }
+});
 
 
 
