a36be9de51285461f4ddb92823fd51839d297d89
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2019-05-02 21:24:18 -0700

    use token and GET user from mongo

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../../middleware/auth')
+const bcrypt = require('bcrypt');
+const auth = require('../../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const {check, validationResult} = require('express-validator/check');
+
 
 const User = require('../../models/User');
 
@@ -18,5 +23,55 @@ router.get('/', auth, async (req, res) => {
 });
 
 
+//@route POST api/auth
+//@discription authenticate user and get token
+//@access public
+router.post(
+  '/', 
+  [
+    check('email', 'please include a valid email').isEmail(),
+    check('password', 'password is required').exists()
+  ], 
+  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({ errors: errors.array() });
+  }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+ //see if user exists
+      if (!user) {
+        return res
+         .status(400)
+         .json({ errors: [{ msg: 'invalid credentials'}] });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if(!isMatch) {
+        return res.status(400)
+          .json({ errors: [{ msg: 'invalid credentials'}] });
+      }
+  const payload = {
+    user: {
+      id: user.id
+    }
+  };
+
+  jwt.sign(
+    payload, 
+    config.get('jwtSecret'), 
+    { expiresIn: 36000 },
+    (err, token) => {
+      if (err) throw err;
+      res.json({token});
+  });
+
+    } catch (err) {
+       console.error(err.message);
+       res.status(500).send('server error');
+    }
+ 
+  }
+);
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {check, validationResult} = require('express-validator/check');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const {check, validationResult} = require('express-validator/check');
 
 const User = require('../../models/User');
 
