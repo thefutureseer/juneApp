c7b03ad70bfe9b64fe09ac28f063026feba7071f
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2019-05-15 16:06:41 -0700

    add comment on a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -86,7 +86,7 @@ router.get('/:id', auth, async (req, res) => {
 //@access private
 router.delete('/:id', auth, async (req, res) => {
   try {
-   const post = await Post.findById( req.params.id );
+   const post = await Post.findById(req.params.id);
 
    if(!post) {
     return res.status(404).json({ msg: 'post not found '});
@@ -110,5 +110,48 @@ router.delete('/:id', auth, async (req, res) => {
   }
  });
  
+
+
+//@route POST api/posts/comment/:id
+//@discription create a comment on a post
+//@access private
+router.post(
+  '/comment/:id', 
+  [ 
+    auth, 
+    [ 
+      check('text', 'text is required')
+        .not()
+        .isEmpty()
+    ]
+  ], 
+  async (req, res) => {
+   const errors = validationResult(req);
+   if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+   }
+   try {
+    const user = await User.findById(req.user.id).select('-password');
+    const post = await Post.findById(req.params.id);
+    
+    const newComment = {
+      text: req.body.text,
+      name: user.name,
+      user: req.user.id
+    };
+
+    post.comments.unshift(newComment);
+
+    await post.save();
+
+    res.json(post.comments);
+   } catch (err) {
+     console.error(err.message);
+     res.status(500).send('server error');
+   }
+  
+  }
+);
+
 
 module.exports = router;
