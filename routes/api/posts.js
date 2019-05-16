1c1aa58430dc51ead402502601b3913b0537dc88
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2019-05-15 17:13:49 -0700

    user delete own comment on a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -153,5 +153,38 @@ router.post(
   }
 );
 
+//@ route DELETE api/posts/comment/:id/comment_:id
+//@ description delete a comment
+//@ private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+ try {
+   const post = await Post.findById(req.params.id);
+
+//pull out comment
+   const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+
+//make sure comment exists
+   if(!comment) {
+     return res.status(404).json({msg: 'comment does not exist'});
+   }
+
+//check user
+   if(comment.user.toString() !== req.user.id) {
+    return res.status(401).json({msg: 'User not authorized'});
+   }
+   const removeIndex = post.comments
+    .map(comment => comment.user.toString())
+    .indexOf(req.user.id);
+   
+   post.comments.splice(removeIndex, 1);
+
+   await post.save();
+
+   res.json(post.comments);
+ } catch (err) {
+   console.error(err.message);
+    res.status(500).send('server error');
+ }
+})
 
 module.exports = router;
