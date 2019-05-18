8a84110bf5abbe42df96ba679dbde7b0a41b321a
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2019-05-18 15:17:32 -0700

    register and login form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import {Link} from 'react-router-dom';
 
 const Login = () => {
- return <div>Login</div>;
+  const [ formData, setFormData ] = useState({
+    name: '',
+    phone: '',
+    password: ''
+  });
+
+  const {name, phone, password} = formData;
+
+  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
+
+  const onSubmit = async e => { 
+    e.preventDefault();
+    console.log('success');
+  };
+
+  return <Fragment>
+  <h1 className="large text-primary">Sign Up</h1>
+  <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+  <form className="form" onSubmit={e => onSubmit(e)}>
+    <div className="form-group">
+      <input 
+        type="text" 
+        placeholder="Name" 
+        name="name" 
+        value={name}
+        onChange={e => onChange(e)}
+        required 
+      />
+    </div>
+    <div className="form-group">
+      <input type="phone" placeholder="phone number" name="phone" value={phone} onChange={e => onChange(e)} required/>
+    </div>
+    <div className="form-group">
+      <input
+        type="password"
+        placeholder="Password"
+        name="password"
+        value={password}
+        onChange={e => onChange(e)}
+        required
+        minLength="6"
+      />
+    </div>
+    <input type="submit" className="btn btn-primary" value="Login" />
+  </form>
+  <p className="my-1">
+    Dont already have an account? <Link to="/register">sign up</Link>
+  </p>
+</Fragment>
 };
 
 export default Login;
