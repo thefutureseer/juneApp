2c035117172a15dc90e70a9c6ce790af3c03b6e3
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2019-05-24 11:22:51 -0700

    login user w email password

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -61,3 +61,37 @@ export const register = ({ name, email, password }) => async dispatch => {
     });
   }
 };
+
+
+//login user
+export const login = ({ email, password }) => async dispatch => {
+  const config = { 
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  const body = JSON.stringify({ email, password });
+
+
+
+  try {
+    const res = await axios.post('/api/auth', body, config);
+
+    dispatch({
+     type: LOGIN_SUCCESS,
+     payload: res.data
+    });
+
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if(errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: LOGIN_FAIL
+    });
+  }
+};
