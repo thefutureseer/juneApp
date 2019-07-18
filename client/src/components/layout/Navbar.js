746023187819716f750e7ca69e9b113522ee6e86
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2019-07-17 22:54:27 -0700

    fixed logout bugs

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,14 +4,13 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
 
-const Navbar = ({ auth: isAuthenticated, loading }, logout) => {
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
         <a onClick={ logout } href="#!">
-          <i className='fas fa-sign-out-alt'></i>
-            {' '} 
-              <span className='hide-sm'>Logout</span>
+          <i className='fas fa-sign-out-alt'/>{' '} 
+          <span className='hide-sm'>Logout</span>
         </a>
       </li>
     </ul>
