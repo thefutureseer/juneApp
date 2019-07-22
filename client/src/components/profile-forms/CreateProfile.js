ce877c28fa779e77a2ece7a8a6278a73170735b8
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2019-07-22 10:53:45 -0700

    toggle social links

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -18,6 +18,8 @@ const CreateProfile = props => {
     instagram: ""
   });
 
+  const [ displaySocialInputs, toggleSocialInputs ] = useState(false);
+
   const {
     company,
     website,
@@ -39,7 +41,8 @@ const CreateProfile = props => {
         Create Your Profile
       </h1>
       <p className="lead">
-        <i className="fas fa-user"></i> Let's get some information to make your
+        <i className="fas fa-user"></i> 
+        Let's get some information to make your
         profile stand out
       </p>
       <small>* = required field</small>
@@ -56,45 +59,32 @@ const CreateProfile = props => {
             <option value="Intern">Intern</option>
             <option value="Other">Other</option>
           </select>
-          <small className="form-text"
-            >Give us an idea of where you are at in your career</small
-          >
+          <small className="form-text">Give us an idea of where you are at in your career</small>
         </div>
         <div className="form-group">
           <input type="text" placeholder="Company" name="company" />
-          <small className="form-text"
-            >Could be your own company or one you work for</small
-          >
+          <small className="form-text">Could be your own company or one you work for</small>
         </div>
         <div className="form-group">
           <input type="text" placeholder="Website" name="website" />
-          <small className="form-text"
-            >Could be your own or a company website</small
-          >
+          <small className="form-text" >Could be your own or a company website</small>
         </div>
         <div className="form-group">
           <input type="text" placeholder="Location" name="location" />
-          <small className="form-text"
-            >City & state suggested (eg. Boston, MA)</small
-          >
+          <small className="form-text">City and state suggested (eg. Boston, MA)</small>
         </div>
         <div className="form-group">
           <input type="text" placeholder="* Skills" name="skills" />
-          <small className="form-text"
-            >Please use comma separated values (eg.
-            HTML,CSS,JavaScript,PHP)</small
-          >
+          <small className="form-text">Please use comma separated values (eg. HTML, CSS, JavaScript, PHP)</small>
         </div>
         <div className="form-group">
           <input
             type="text"
             placeholder="Github Username"
-            name="githubusername"
-          />
+            name="githubusername"/>
           <small className="form-text"
             >If you want your latest repos and a Github link, include your
-            username</small
-          >
+            username</small>
         </div>
         <div className="form-group">
           <textarea placeholder="A short bio of yourself" name="bio"></textarea>
@@ -102,39 +92,43 @@ const CreateProfile = props => {
         </div>
 
         <div className="my-2">
-          <button type="button" className="btn btn-light">
+          <button onClick={() => toggleSocialInputs(!displaySocialInputs)} type="button" className="btn btn-light">
             Add Social Network Links
           </button>
           <span>Optional</span>
         </div>
+          {displaySocialInputs && 
+          <Fragment>
 
-        <div className="form-group social-input">
-          <i className="fab fa-twitter fa-2x"></i>
-          <input type="text" placeholder="Twitter URL" name="twitter" />
-        </div>
+            <div className="form-group social-input">
+              <i className="fab fa-twitter fa-2x"></i>
+              <input type="text" placeholder="Twitter URL" name="twitter" />
+            </div>
 
-        <div className="form-group social-input">
-          <i className="fab fa-facebook fa-2x"></i>
-          <input type="text" placeholder="Facebook URL" name="facebook" />
-        </div>
+            <div className="form-group social-input">
+              <i className="fab fa-facebook fa-2x"></i>
+              <input type="text" placeholder="Facebook URL" name="facebook" />
+            </div>
 
-        <div className="form-group social-input">
-          <i className="fab fa-youtube fa-2x"></i>
-          <input type="text" placeholder="YouTube URL" name="youtube" />
-        </div>
+            <div className="form-group social-input">
+              <i className="fab fa-youtube fa-2x"></i>
+              <input type="text" placeholder="YouTube URL" name="youtube" />
+            </div>
 
-        <div className="form-group social-input">
-          <i className="fab fa-linkedin fa-2x"></i>
-          <input type="text" placeholder="Linkedin URL" name="linkedin" />
-        </div>
+            <div className="form-group social-input">
+              <i className="fab fa-linkedin fa-2x"></i>
+              <input type="text" placeholder="Linkedin URL" name="linkedin" />
+            </div>
 
-        <div className="form-group social-input">
-          <i className="fab fa-instagram fa-2x"></i>
-          <input type="text" placeholder="Instagram URL" name="instagram" />
-        </div>
+            <div className="form-group social-input">
+              <i className="fab fa-instagram fa-2x"></i>
+              <input type="text" placeholder="Instagram URL" name="instagram" />
+            </div>
+          </Fragment>}
         <input type="submit" className="btn btn-primary my-1" />
         <a className="btn btn-light my-1" href="dashboard.html">Go Back</a>
       </form>
+
      </Fragment> 
   );
 };
