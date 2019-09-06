f119cfcc3c29535a331eb06589929d9cf1774573
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2019-09-05 17:34:45 -0700

    add experience

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from "../actions/types";
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from "../actions/types";
 
 const initialState = {
   profile: null,
@@ -17,7 +17,13 @@ export default function(state = initialState, action) {
       ...state,
       profile: payload,
       loading: false
-    }
+    };
+  case UPDATE_PROFILE:
+      return {
+        ...state,
+        profile: payload,
+        loading: false
+      };
   case PROFILE_ERROR:
     return {
       ...state,
