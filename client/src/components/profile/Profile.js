b2c6bd3b57f9ae2cbf29064e87917e6626af2333
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2019-12-20 19:08:40 -0800

    github repos showing on browser

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -25,14 +25,14 @@ const Profile = ({
           profile === null || loading ? ( <Spinner />) : 
           (
           <Fragment> 
-            <Link to="/profiles" clasName="btn btn-light">Back to all profiles</Link>
+            <Link to="/profiles" clasname="btn btn-light">Back to all profiles</Link>
             { 
               auth.isAuthenticated 
                 && 
               auth.loading === false 
                 && 
               auth.user._id === profile.user._id 
-                && ( <Link to="/edit-profile" className="btn btn-dark">Edit Profile</Link> )
+                && ( <Link to="/edit-profile" classname="btn btn-dark">Edit Profile</Link> )
             }
               <div className="profile-grid my-1">
                 <ProfileTop profile={profile} />
