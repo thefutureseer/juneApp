a338168bd632580bba3e922ab41fa56c728ba478
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2019-12-22 21:05:02 -0800

    frontend posts

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,18 +2,26 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
 import { getPosts } from '../../actions/post';
 
 const Posts = ({ getPosts, post: {posts, loading}}) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  return (
-    <div>
-      Hello Post World!!
-    </div>
-  )
-}
+  return loading ? ( <Spinner />) : (<Fragment>
+       <h1 className="large text-primary">Posts</h1>
+       <p className="lead"><i className="fas fa-user">Welcome to FG!</i></p>
+
+       {/* POST FORM */}
+       <div className="posts">
+          {posts.map(post => (
+            <PostItem key={post._id} post={post}/>
+          ))}
+       </div>
+  </Fragment>
+    );
+};
 
 Posts.propTypes = {
   getPosts: PropTypes.func.isRequired,
@@ -23,6 +31,6 @@ Posts.propTypes = {
 
 const mapStateToProps = state => ({
   post: state.post
-})
+});
 
 export default connect(mapStateToProps, { getPosts })(Posts);
