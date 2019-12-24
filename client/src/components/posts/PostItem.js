cef109f3ba414d23e76c7d027f8c21d892b7d294
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2019-12-23 16:06:32 -0800

    make post more dynamic ready for likes

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
 
-const PostItem = ({auth, post:{_id, text, name, avatar, user, likes, comments, date}}) => 
+const PostItem = ({auth, post:{_id, text, name, avatar, user, likes, comments, date}}) => (
   <div class="post bg-white p-1 my-1">
     <div>
       <a href="profile.html">
@@ -12,37 +12,40 @@ const PostItem = ({auth, post:{_id, text, name, avatar, user, likes, comments, d
           class="round-img"
           src={avatar}
           alt=""/>
-        <h4>Martha</h4>
+        <h4>{name}</h4>
       </a>
     </div>
     <div>
       <p class="my-1">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-        possimus corporis sunt necessitatibus! Minus nesciunt soluta
-        suscipit nobis. Amet accusamus distinctio cupiditate blanditiis
-        dolor? Illo perferendis eveniet cum cupiditate aliquam?
+       {text}
       </p>
         <p class="post-date">
-          Posted on 05/16/2019
+          Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
       </p>
       <button type="button" class="btn btn-light">
-        <i class="fas fa-thumbs-up"></i>
-        <span>4</span>
+        <i class="fas fa-thumbs-up"/> {' '}
+        <span>{likes.length > 0 && (
+          <span>{likes.length}</span>
+        ) }</span>
       </button>
       <button type="button" class="btn btn-light">
         <i class="fas fa-thumbs-down"></i>
       </button>
-      <a href="post.html" class="btn btn-primary">
-        Discussion <span class='comment-count'>2</span>
-      </a>
+      <Link to={`/post/${_id}`} class="btn btn-primary">
+        Discussion{' '} {comments.length > 0 && (
+          <span class='comment-count'>{comments.length}</span>
+        ) } 
+      </Link>
+      {!auth.loading && user === auth.user._id && (
       <button      
         type="button"
         class="btn btn-danger" >
-      <i class="fas fa-times"></i>
-    </button>
+        <i class="fas fa-times"></i>
+      </button>  
+      )}
     </div>
   </div> 
-  
+); 
 
 PostItem.propTypes = {
  post: PropTypes.object.isRequired,
