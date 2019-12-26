f5ea9b4358d31d6bcfb7a1490335af119d31d03e
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2019-12-26 15:02:54 -0800

    add and remove likes delete users own posts

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -5,10 +5,11 @@ import Spinner from '../layout/Spinner';
 import PostItem from './PostItem';
 import { getPosts } from '../../actions/post';
 
-const Posts = ({ getPosts, post: {posts, loading}}) => {
+const Posts = ({ getPosts, post: {posts, loading} }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
+
   return loading ? ( <Spinner />) : (<Fragment>
        <h1 className="large text-primary">Posts</h1>
        <p className="lead"><i className="fas fa-user">Welcome to FG!</i></p>
@@ -16,7 +17,7 @@ const Posts = ({ getPosts, post: {posts, loading}}) => {
        {/* POST FORM */}
        <div className="posts">
           {posts.map(post => (
-            <PostItem key={post._id} post={post}/>
+            <PostItem key={post._id} post={post} />
           ))}
        </div>
   </Fragment>
